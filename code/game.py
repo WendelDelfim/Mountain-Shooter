4c412dae4892ffacd48900235b640841a0bc22fd
Wendel Delfim <wendelcode1@gmail.com>
2025-07-07 15:44:18 -0300

    Creating main loop Connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame   #0__Importando o pygame
+from code.menu import Menu  #0__ Importando Menu.py, para que ele consiga ser utilizado na while true
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()  # Ativando o pygame
+        self.window = pygame.display.set_mode(size=(600, 480))  # Definir o tamanho da tela
 
-    def run(self, ):
-        pass
+    def run(self):
+        while True: #Fazendo com que a janela abrá, antes não estava respondendo.
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
+            # Check for all events
+            # for event in pygame.event.get():
+            #   if event.type == pygame.QUIT:
+            #        pygame.quit() #Close Window
+            #           quit() #end pygame
+
--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame #Importando o pygame
+from code.game import Game  #0__ Importando o Game.py
 
-print("Setup Start") #Para depurar, saber onde o código está abrindo para identificar algum erro
-pygame.init() #Ativando o pygame
-window = pygame.display.set_mode(size=(600, 480)) #Definir o tamanho da tela
-print("Setup end") #Para depurar, saber onde o código está fechando, para identificar algum erro.
-
-print("Loop Start")
-while True: #Fazendo com que a janela abrá, antes não estava respondendo.
-    #Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Close Window
-            quit() #end pygame
-
+game = Game ()  #0__ Trazendo a classe Game, do outro arquivo aqui para o main.py
+game.run()  #0__ Fazendo com que rode o codigo aqui em main.py
